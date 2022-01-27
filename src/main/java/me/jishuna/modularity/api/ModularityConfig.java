781f400ee8fa1359321a0fb1e14caf9cb7c77e96
coll1234567 <joshl5324@gmail.com>
2022-01-27 17:34:33 -0500

    Yay comments

--- src/main/java/me/jishuna/modularity/api/ModularityConfig.java
+++ src/main/java/me/jishuna/modularity/api/ModularityConfig.java
@@ -1,9 +1,11 @@
 package me.jishuna.modularity.api;
 
+import redempt.redlib.config.annotations.Comment;
 import redempt.redlib.config.annotations.ConfigName;
 
 public class ModularityConfig {
-	
+
+	@Comment("The default language to use for new players, will also be used as a fallback when another language is missing a translation.")
 	@ConfigName("default-language")
 	public static String defaultLanguage = "en_us";
 
